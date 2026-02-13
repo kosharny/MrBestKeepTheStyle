aac3dfd07d0a28197f46829f9217f4b9e2f8dbde
Maksim Kosharny <maksimkosharnyjj@gmail.com>
2026-02-13 19:19:21 +0200

    Initial Commit

---
+++ MrBestKeepTheStyle/MrBestKeepTheStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MrBestKeepTheStyleApp.swift
+//  MrBestKeepTheStyle
+//
+//  Created by Maksim Kosharny on 13.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct MrBestKeepTheStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
